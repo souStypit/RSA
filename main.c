09bc5068340c54bdf55bd86a5c8afbb8d25435d7
souStypit <s4venkov1lya@gmail.com>
2023-11-18 02:00:52 +0300

    доделал сканф предложений

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <gmp.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
 
 #define SIZE 8192
 
@@ -27,6 +28,11 @@ void encrypt(mpz_t e, mpz_t n, char message[SIZE]) {
     mpz_inits(messageToInt, encryptedMessage, NULL);
 
     for (int i = 0; message[i] != '\0'; i++) {
+        if (message[i] == ' ') {
+            fprintf(stream, "\n");
+            continue;
+        } 
+
         r[i] = (int)message[i];
         mpz_set_ui(messageToInt, r[i]);
         mpz_powm(encryptedMessage, messageToInt, e, n);
@@ -39,22 +45,37 @@ void encrypt(mpz_t e, mpz_t n, char message[SIZE]) {
 
 void decrypt(mpz_t d, mpz_t n, char (*resultPtr)[SIZE]) {
     FILE *stream = fopen("result.txt", "r");
-    char *res = *resultPtr;
-    char buffer[SIZE];
+    char *res = *resultPtr, buffer[SIZE];
+    int i = 0;
     mpz_t encryptedMessage, decryptedMessage;
     mpz_inits(encryptedMessage, decryptedMessage, NULL);
 
-    for (int i = 0; !feof(stream); i++) {
+    for (i = 0; !feof(stream); i++) {
         if (fgets(buffer, SIZE, stream) == NULL) break;
+        if (!strcmp(buffer, "\n")) {
+            res[i] = ' ';
+            continue;
+        }
         
         mpz_set_str(encryptedMessage, buffer, 10);
 
         mpz_powm(decryptedMessage, encryptedMessage, d, n);
         res[i] = (char)mpz_get_ui(decryptedMessage);
     }
+    res[i] = '\0';
 
     mpz_clears(encryptedMessage, decryptedMessage, NULL);
     fclose(stream);
+}
+
+void scanf_sequence(char (*resultPtr)[SIZE]) {
+    char ch, *res = *resultPtr;
+    int i = 0;
+
+    while ((ch = getchar()) != '\n') {
+        res[i++] = ch; 
+    }
+    res[i] = '\0';
 }
 
 int main() {
@@ -65,6 +86,7 @@ int main() {
 
     printf("Введите степень 2 (от 10 до 16 (после 13 - медленно)): ");
     scanf("%d", &power);
+    getchar();
     if (power > 16 || power < 10) return 1;
     
     mpz_inits(p, eulerP, q, eulerQ, n, euler, e, d, i, NULL);
@@ -98,7 +120,8 @@ int main() {
 
     //RSA algorithm
     printf("\nВведите сообщение: ");
-    scanf("%s", message);
+    scanf_sequence(&message);
+    printf("%s\n", message);
 
     encrypt(e, n, message);
     decrypt(d, n, &decryptedMessage);
